43bbb608aa4daa7759f3950ecca656307b0162d8
Rafael Vinicius Garcia <rafael_garcia1993@yahoo.com.br>
2021-07-31 16:13:17 -0300

    Testado GitHub com PyCharm.
    Fiz um pequeno código para testar meu GitHub com o Pycharm.

---
+++ CalculoTeste.py
@@ -0,0 +1,10 @@
+print("Vamos testar digitando um número ok?")
+numero1 = int(input('Digite um número por favor.'))
+numero2 = int(input('Digite outro número por favor.'))
+media = numero1 + numero2
+print('O resultado da média é {}.'.format(media))
+
+
+#Ok, este foi um simples código para testar o Pycharm com o GitHub.
+
+
